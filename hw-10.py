336b709aa34ff6f4a4be8d241b385224811cc72d
Elliot Beck <elliot.beck@htomail.com>
2023-11-29 12:43:56 +0000

    Remove shrinkage example

--- hw-10.py
+++ hw-10.py
@@ -31,33 +31,3 @@ plt.xlabel("p")
 plt.ylabel("log-likelihood")
 plt.show()
 # The maximum likelihood estimate for p is 5/10 = 0.5
-
-## Wolf Exercise 2
-# Create data
-data = np.array(
-    [
-        [-2.2, -0.2, 0.2, 1.2, -0.2, 0.4, 0.1],
-        [-0.5, 0.7, -0.4, 0.3, 0.7, 1.2, -0.3],
-        [0.4, -0.9, 0.1, 0.8, -1.1, -0.6, 0.5],
-        [0.7, 1.8, -0.2, 0.7, -1.2, 1.9, -1.1],
-    ]
-)
-
-# Calculate the grand mean and shrinkage mean estimator for various alphas
-grand_mean = np.mean(np.mean(data, axis=1))
-alphas = [0, 0.25, 0.5, 0.75, 1]
-mean_shrinkage = [
-    alpha * grand_mean + (1 - alpha) * np.mean(data, axis=1) for alpha in alphas
-]
-
-## Wolf Exercise 3
-# Find the optimal alpha according to Stein, Page 38, Slides 08
-sigma_hat = (1 / (data.shape[0] - 1)) * np.sum((data.T - np.mean(data, axis=1)) ** 2)
-alpha_star = (
-    (data.shape[0] - 3) * sigma_hat / np.sum((data.T - np.mean(data, axis=1)) ** 2)
-)
-
-# Calculate the shrinkage mean estimator for the optimal alpha
-mean_shrinkage_optimal = alpha_star * grand_mean + (1 - alpha_star) * np.mean(
-    data, axis=1
-)
